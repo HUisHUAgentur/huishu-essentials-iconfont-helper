d87f5ab2ee984766a2cf5ab9d131fd5b21bbb132
HUisHU-Sebastian <58956016+HUisHU-Sebastian@users.noreply.github.com>
2023-08-29 15:26:23 +0200

    Update huishu-essentials-iconfont-helper.php

--- huishu-essentials-iconfont-helper.php
+++ huishu-essentials-iconfont-helper.php
@@ -5,7 +5,7 @@
  * Version:           2.1
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            HUisHU. Digitale Kreativagentur.
+ * Author:            HUisHU. Digitale Kreativagentur GmbH
  * Author URI:        https://www.huishu-agentur.de
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
